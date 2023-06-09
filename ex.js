5f381ac40956f3229733453740353173c0f4650b
성영욱 <sung@seong-yeong-ug-ui-MacBookAir.local>
2023-06-09 16:11:46 +0900

    예제 만들어보기

---
+++ ex.js
@@ -0,0 +1,13 @@
+var car = ["sonata","avante","matiz"];
+
+car[1] = "sm5";
+
+console.log(car.reverse());
+
+var person ={
+    firstName : "길동",
+    lastName : "홍",
+    country : "Korea"
+}
+
+console.log(person.lastName + person.firstName + "의 고향은 " + person.country + "입니다.");
